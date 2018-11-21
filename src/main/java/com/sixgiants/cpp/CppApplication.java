920223b77b562fe9dffba483ee49deeba2253239
Lijiajun <1627851906@qq.com>
2018-11-21 12:35:48 +0800

    项目部署...

---
+++ src/main/java/com/sixgiants/cpp/CppApplication.java
@@ -0,0 +1,12 @@
+package com.sixgiants.cpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CppApplication.class, args);
+    }
+}
