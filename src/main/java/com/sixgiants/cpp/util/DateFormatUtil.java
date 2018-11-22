d2309761812b23a56aaff1ca17a132078e743551
gmuk <1427600836@qq.com>
2018-11-22 17:09:17 +0800

    增加字符串转时间

--- src/main/java/com/sixgiants/cpp/util/DateFormatUtil.java
+++ src/main/java/com/sixgiants/cpp/util/DateFormatUtil.java
@@ -1,5 +1,6 @@
 package com.sixgiants.cpp.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -11,5 +12,14 @@ public class DateFormatUtil {
     public static String dateFormat(Date date){
         SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return simpleDateFormat.format(date);
+    }
+    public static Date StringToDate(String date){
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return simpleDateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
