a9f4ac526c8ebfcaa75389ec13fba11fcf5a958e
zpf <1064203549@qq.com>
2018-12-05 12:05:46 +0800

    登录、注册（第二版）

--- src/main/java/com/sixgiants/cpp/controller/UserController.java
+++ src/main/java/com/sixgiants/cpp/controller/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,14 +21,50 @@ public class UserController {
     @Autowired
     private UserServiceImpl userServiceImpl;
 
-    @PostMapping("/login_action")
-    public String login(String name, String password,Model model,HttpServletRequest request) {
-        return userServiceImpl.login(name,password,model,request);
+    @PostMapping("/confirmName")
+    @ResponseBody
+    public int confirmName(@RequestBody String username){
+        return userServiceImpl.confirmName(username);
+    }
+    @PostMapping("/login")
+    @ResponseBody
+    public String login(@RequestBody String[] message,HttpServletRequest request,Model model){
+        return userServiceImpl.login(message,request,model);
     }
 
+    @RequestMapping("/login_action")
+    public String login_action(HttpSession session,Model model){
+        User user = (User)session.getAttribute("user");
+        model.addAttribute("user",user);
+        return "success.html";
+    }
+
+    @PostMapping("/confirmPassword")
+    @ResponseBody
+    public int confirmPassword(@RequestBody String[] message){
+        return userServiceImpl.confirmPassword(message);
+    }
+
+    @PostMapping("/confirmEmail")
+    @ResponseBody
+    public int confirmEmail(@RequestBody String email){
+        return userServiceImpl.confirmEmail(email);
+    }
+
+    @PostMapping("/confirmPhone")
+    @ResponseBody
+    public int confirmPhone(@RequestBody String phone){
+        return userServiceImpl.confirmPhone(phone);
+    }
+
+//    @PostMapping("/login_action")
+//    public String login(String name, String password,Model model,HttpServletRequest request) {
+//        return userServiceImpl.login(name,password,model,request);
+//    }
+
     @PostMapping("/register")
-    public String register(User user, HttpServletRequest request){
-        return userServiceImpl.register(user,request);
+    public String register(User user){
+        return userServiceImpl.register(user);
     }
 
     @GetMapping("/update")
@@ -43,6 +76,7 @@ public class UserController {
     public String updateUser(User user,Model model,HttpServletRequest request){
         return userServiceImpl.updateUser(user,model,request);
     }
+
 
     @GetMapping("/loginOut")
     public String loginOut(HttpSession session){
--- src/main/java/com/sixgiants/cpp/service/impl/UserServiceImpl.java
+++ src/main/java/com/sixgiants/cpp/service/impl/UserServiceImpl.java
@@ -8,58 +8,77 @@ import com.sixgiants.cpp.util.UUIDutil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserServiceImpl {
     @Autowired
     private UserDao userDao;
 
-    public String login(String name, String password,Model model,HttpServletRequest request) {
-        String passwordMD5 = MD5Util.md5(password);
-        User user = userDao.findByNameAndPsw(name, passwordMD5);
-        if (user!= null) {
-            System.out.println("登录成功！");
-            request.getSession().setAttribute("user",user);
-            model.addAttribute("user",user);
-            return "success.html";
-        }else {
-            if (userDao.findByName(name)!=null){
-                System.out.println("登录失败！");
-                return "redirect:/login.html";
-            }else {
-                return "sign.html";
-            }
+    public int confirmName(String username){
+        User user = userDao.findByName(username);
+        if(user==null){
+            return 1;
+        }
+        else{
+            return 0;
         }
     }
 
-    public String register(User user,HttpServletRequest request){
-        String rePassword = request.getParameter("rePassword");
-        String username = user.getName();
-        if (userDao.findByName(username)==null){
-            if (!rePassword.equals(user.getPassword())){
-                System.out.println("密码不一致");
-                return "redirect:/register.html";
-            }
-            if (!user.getEmail().matches("\\b^['_a-z0-9-\\+]+(\\.['_a-z0-9-\\+]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*\\.([a-z]{2}|aero|arpa|asia|biz|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|nato|net|org|pro|tel|travel|xxx)$\\b")){
-                System.out.println("邮箱格式错误");
-                return "redirect:/register.html";
-            }
-
-            user.setPassword(MD5Util.md5(user.getPassword()));
-            user.setCreateTime(new Date());
-            userDao.saveUser(user);
-            System.out.println("注册成功");
-            return "redirect:/login.html";
+    public String login(String message[],HttpServletRequest request,Model model){
+        String passwordMD5 = MD5Util.md5(message[1]);
+        User user = userDao.findByNameAndPsw(message[0], passwordMD5);
+        if (user!=null){
+            request.getSession().setAttribute("user",user);
+            model.addAttribute("user",user);
+            return "success";
         }else {
-            System.out.println("该用户已经存在");
-            return "redirect:/register.html";
+            return "not";
         }
+    }
+
+    public int confirmPassword(String message[]){
+        String password = message[0];
+        String rePassword = message[1];
+        if (password.equals(rePassword)){
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+
+    public int confirmEmail(String email){
+        if (!email.matches("\\b^['_a-z0-9-\\+]+(\\.['_a-z0-9-\\+]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*\\.([a-z]{2}|aero|arpa|asia|biz|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|nato|net|org|pro|tel|travel|xxx)$\\b")){
+            return 0;
+        }else {
+            return 1;
+        }
+    }
+
+    public int confirmPhone(String phone){
+        if (phone.length()!=11){
+            return 0;
+        }else {
+            return 1;
+        }
+    }
+
+
+    public String register(User user){
+        user.setPassword(MD5Util.md5(user.getPassword()));
+        user.setCreateTime(new Date());
+        userDao.saveUser(user);
+        return "redirect:/login.html";
     }
 
     public String toUpdate(HttpServletRequest request, Model model){
@@ -80,8 +99,6 @@ public class UserServiceImpl {
         user.setCreateTime(date.getCreateTime());
         user.setPassword(MD5Util.md5(user.getPassword()));
         userDao.updateUser(user);
-        System.out.println("修改成功");
-
         model.addAttribute("user",user);
 
         return "success.html";
